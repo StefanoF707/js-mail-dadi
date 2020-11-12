5f202847f3e1d78bde58ac7ad080073e29e0f233
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-12 15:33:40 +0100

    gioco dei dadi

--- dadi/js/gioco_dei_dadi.js
+++ dadi/js/gioco_dei_dadi.js
@@ -1,3 +1,8 @@
+// Gioco dei dadi, chi fa di più vince.
+// Generare un numero random da 1 a 6, sia per il
+// giocatore sia per il compute. Stabilire il
+// vincitore, in base a chi fa il punteggio più alto.
+
 // creazione numero randomico giocatore 1
 var humanPlayer = Math.floor((Math.random() * 6) + 1);
 console.log(humanPlayer);
