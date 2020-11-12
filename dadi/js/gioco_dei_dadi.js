6ddc8db7b1f0089bd815959c2a8d4f2beeb82240
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-12 16:47:12 +0100

    mail

--- dadi/js/gioco_dei_dadi.js
+++ dadi/js/gioco_dei_dadi.js
@@ -5,12 +5,12 @@
 
 // creazione numero randomico giocatore 1
 var humanPlayer = Math.floor((Math.random() * 6) + 1);
-console.log(humanPlayer);
+// console.log(humanPlayer);
 document.getElementById("human").innerHTML = humanPlayer;
 
 // creazione numero randomico giocatore 2
 var computerPlayer = Math.floor((Math.random() * 6) + 1);
-console.log(computerPlayer);
+// console.log(computerPlayer);
 document.getElementById("computer").innerHTML = computerPlayer;
 
 // determinare il vincitore
@@ -19,5 +19,5 @@ if (humanPlayer > computerPlayer) {
 } else if (humanPlayer < computerPlayer) {
      document.getElementById("winner").innerHTML = "Computer Player!"
 } else {
-     document.getElementById("winner").innerHTML = "Pareggio!"
+     document.getElementById("winner").innerHTML = "Draw!"
 }
--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,3 +1,24 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato
+
+// chiedere l'email all'utente
+var userEmail = prompt("Prego inserisca la sua email:");
+
+// creare una lista di mail
+var mailList = ["first.mail@gmail.com", "second.mail@gmail.com", "third.mail@gmail.com", "fourth.mail@gmail.com"];
+
+// navigare nella array delle mail per cercare la corrispondenza
+var result = false;
+for (var i = 0; i < mailList.length; i++) {
+     if (userEmail === mailList[i]) {
+          result = true;
+     }
+}
+
+// se l'email è nella lista consentire l'accesso, altrimenti negare
+if (result === true) {
+     document.getElementById("access").innerHTML = "Accesso consentito!";
+} else {
+     document.getElementById("access").innerHTML = "Accesso negato!";
+}
