7975962acfecc76f6040ba6979f50dc09b2847cc
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-12 14:36:54 +0100

    commit iniziale

---
+++ dadi/js/gioco_dei_dadi.js
@@ -0,0 +1,4 @@
+// Gioco dei dadi, chi fa di più vince.
+// Generare un numero random da 1 a 6, sia per il
+// giocatore sia per il compute. Stabilire il
+// vincitore, in base a chi fa il punteggio più alto.
